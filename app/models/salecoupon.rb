cf722070e884cbd7a7c2538ac67a7074825cbba8
perunari <per.roc.ski@gmail.com>
2020-09-26 12:59:26 +0900

    CreateSalecouponModel

---
+++ app/models/salecoupon.rb
@@ -0,0 +1,2 @@
+class Salecoupon < ApplicationRecord
+end
---
+++ db/migrate/20200926035717_create_salecoupons.rb
@@ -0,0 +1,8 @@
+class CreateSalecoupons < ActiveRecord::Migration[5.2]
+  def change
+    create_table :salecoupons do |t|
+
+      t.timestamps
+    end
+  end
+end
