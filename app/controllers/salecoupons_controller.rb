19835afaf58239a4e056ce203ee0f50f04ecf9b8
perunari <per.roc.ski@gmail.com>
2020-10-06 01:48:34 +0900

    一旦コミット（デバイス無効化中）

--- app/controllers/salecoupons_controller.rb
+++ app/controllers/salecoupons_controller.rb
@@ -1,7 +1,19 @@
 class SalecouponsController < ApplicationController
   def index
+    @salecoupons = Salecoupon.all
   end
 
   def new
+    @salecoupon = Salecoupon.new
   end
+
+  def create
+    Salecoupon.create(salecoupon_params)
+  end
+
+  private
+  def salecoupon_params
+    params.require(:salecoupon).permit(:start, :end, :body)
+  end
+
 end
