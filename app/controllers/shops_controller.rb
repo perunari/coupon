dc23bc14effd3e2cc2d9cb9f9e73f2d637bb2a85
perunari <per.roc.ski@gmail.com>
2020-10-13 13:18:51 +0900

    bootstrap

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,6 +1,8 @@
 class ShopsController < ApplicationController
+  PER = 4
   def show
     @shop = Shop.find(params[:id])
-    @salecoupons = @shop.salecoupons
+    # @salecoupons = @shop.salecoupons
+    @salecoupons = @shop.salecoupons.page(params[:page]).per(PER)
   end
 end
